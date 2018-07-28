5e96ded24afbae2ec6f823205adc07315d9255e6
Armando Sierra <armando.sierra.fimbres@gmail.com>
2018-07-27 17:29:21 -0700

    validation register name

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require("jsonwebtoken");
 const keys = require("./../../config/keys");
 const passport = require("passport");
 
+//Load input validation
+const validateRegisterInput = require("./../../validation/register");
+
 //Load User Model
 const User = require("./../../models/User");
 
@@ -15,6 +18,13 @@ const User = require("./../../models/User");
 router.get("/test", (req, res) => res.json({ msg: "Users works" }));
 
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  //check validation
+  if (!isValid) {
+    return res.status(400).json({ errors });
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
       return res.status(400).json({ email: "Email already exists" });
