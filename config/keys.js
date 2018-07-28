d9fc20bd9c53de86dc1dab66f19624bb507a7c7d
Armando Sierra <armando.sierra.fimbres@gmail.com>
2018-07-27 17:06:17 -0700

    Authorization

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://asierra:a123456@ds253871.mlab.com:53871/devconnector_test"
+    "mongodb://asierra:a123456@ds253871.mlab.com:53871/devconnector_test",
+  secretOrKey: "123456789"
 };
