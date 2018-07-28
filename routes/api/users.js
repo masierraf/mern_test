95df2938d782abf4ae6d155b6d7c60e3b326fb86
Armando Sierra <armando.sierra.fimbres@gmail.com>
2018-07-27 22:40:35 -0700

    validation of login and register done

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require("passport");
 
 //Load input validation
 const validateRegisterInput = require("./../../validation/register");
+const validateLoginInput = require("./../../validation/login");
 
 //Load User Model
 const User = require("./../../models/User");
@@ -59,6 +60,13 @@ router.post("/register", (req, res) => {
 //@description  Login User / Returning JWT token
 //@access       Public
 router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  //check validation
+  if (!isValid) {
+    return res.status(400).json({ errors });
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
@@ -67,7 +75,8 @@ router.post("/login", (req, res) => {
   User.findOne({ email }).then(user => {
     //check fo user
     if (!user) {
-      return res.status(404).json({ email: "user not found" });
+      errors.email = "User not found";
+      return res.status(404).json(errors);
     }
 
     //check Password
@@ -92,7 +101,8 @@ router.post("/login", (req, res) => {
           }
         );
       } else {
-        return res.status(400).json({ password: "Password incorrect!" });
+        errors.password = "Password incorrect";
+        return res.status(400).json(errors);
       }
     });
   });
---
+++ validation/login.js
@@ -0,0 +1,28 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validateLoginInput(data) {
+  let errors = {};
+
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "format of email is incorrect";
+  }
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  }
+
+  if (!Validator.isLength(data.password, { min: 6, max: 32 })) {
+    errors.password = "the password must be between 6 and 32 characters";
+  }
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
