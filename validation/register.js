cbe58686461aca858c17697d2e1ad679502256ed
Armando Sierra <armando.sierra.fimbres@gmail.com>
2018-07-27 18:39:01 -0700

    validation of email

--- validation/register.js
+++ validation/register.js
@@ -7,6 +7,9 @@ module.exports = function validateRegisterInput(data) {
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
     errors.name = "Name must be between 2 and 30 characters";
   }
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "format is incorrect";
+  }
 
   return {
     errors,
